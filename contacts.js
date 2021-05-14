6e23f5d57ca059e38534f344110843ad27277fac
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-05-14 13:12:31 +0300

    Added function to get contact by ID.

--- contacts.js
+++ contacts.js
@@ -5,12 +5,18 @@ const contactsPath = path.join(__dirname, "db", "contacts.json");
 
 function listContacts() {
   fs.readFile(contactsPath, "utf-8")
-    .then(console.log)
+    .then((contacts) => console.table(JSON.parse(contacts)))
     .catch((error) => console.log(error.message));
 }
 
 function getContactById(contactId) {
-  console.log("getContactById");
+  fs.readFile(contactsPath, "utf-8")
+    .then((contacts) => {
+      const requiredContact =
+        JSON.parse(contacts).find((contact) => contact.id === contactId) || "No contact with this ID found.";
+      console.log(requiredContact);
+    })
+    .catch((error) => console.log(error.message));
 }
 
 function removeContact(contactId) {
